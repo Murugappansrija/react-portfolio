1add9df48af4a19d5daaa8afe06020c011dde080
murugappansrija <srijamurugappan66@gmail.com>
2023-11-03 13:07:34 +0530

    data

--- src/components/WorkcardData.js
+++ src/components/WorkcardData.js
@@ -11,7 +11,7 @@ const ProjectCardData = [
     {
         imgsrc : pro2,
         title : "Social Media App",
-        text : "It's a simple social media app, with many functionalities  user can post,delete,edit a posts.user can add & remove friends",
+        text : "It's a simple social media app, with many functionalities  user can post,delete,edit a posts.user can add & remove friends.",
         view :  "https://vocal-rolypoly-f90348.netlify.app/",
         source : "https://github.com/Murugappansrija/connecting-point-fe.git"
 
@@ -20,7 +20,7 @@ const ProjectCardData = [
     {
         imgsrc : pro3,
         title : "Web Scrapper",
-        text : "The web scraper extracts data from Amazon and Flipkart, offering valuable insights and competitive analysis",
+        text : "The web scraper extracts data from Amazon and Flipkart, offering valuable insights and competitive analysis.",
         view :  "https://transcendent-cocada-05e41d.netlify.app/",
         source : "https://github.com/Murugappansrija/wesfn.git"
 
@@ -28,8 +28,8 @@ const ProjectCardData = [
     },
     {
         imgsrc : pro4,
-        title : "Blog App",
-        text : "This blog app with user authentication, enabling blog creation, updates, and deletions, while also providing access to view other users' blogs",
+        title : "To-Do App",
+        text : " With this to-do app, users can efficiently manage their daily tasks, create lists, update, and delete tasks.",
         view :  "https://lambent-taiyaki-76bd38.netlify.app/",
         source : "https://github.com/Murugappansrija/client.git"
 
@@ -37,12 +37,12 @@ const ProjectCardData = [
     },
     {
         imgsrc : pro5,
-        title : "To-Do App",
-        text : "With this to-do app, users can efficiently manage their daily tasks, create lists, update, and delete tasks",
+        title : " Blog App",
+        text : "This blog app with user authentication, enabling blog creation, updates, and deletions, while also providing access to view other users' blogs.",
         view :  "https://64c8a4c156edae4530c28fc6--frabjous-pony-d5b6a6.netlify.app/",
         source : "https://github.com/Murugappansrija/nike-store.git"
 
-
+       
     }
 ]
 export default  ProjectCardData
